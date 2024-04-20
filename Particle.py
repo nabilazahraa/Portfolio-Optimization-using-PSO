773911cc5c04476b1b8a0d7aa567725bca3df993
Youshay <my07103@st.habib.edu.pk>
2024-04-20 14:00:54 +0500

    Added min risk optimization

--- Particle.py
+++ Particle.py
@@ -11,6 +11,11 @@ def calculate_sharpe_ratio(weights, returns, risk_free_rate):
     # We will maximize Sharpe Ratio by minimizing negative Sharpe Ratio
     return -sharpe_ratio
 
+def calculate_portfolio_volatility(weights, returns):
+    # Calculate the expected portfolio volatility
+    portfolio_volatility = np.sqrt(np.dot(weights.T, np.dot(returns.cov() * 252, weights)))
+    return portfolio_volatility
+
 
 # Implement the PSO algorithm
 class Particle:
@@ -20,11 +25,22 @@ class Particle:
         self.best_position = np.copy(self.position)
         self.best_value = float('inf')
 
-    def evaluate(self, returns, risk_free_rate):
-        value = calculate_sharpe_ratio(self.position, returns, risk_free_rate)
-        if value < self.best_value:
+    def evaluate(self, returns, risk_free_rate, optimize_for='sharpe_ratio'):
+        if optimize_for == '1':
+            value = calculate_sharpe_ratio(self.position, returns, risk_free_rate)
+        elif optimize_for == '2':
+            value = calculate_portfolio_volatility(self.position, returns)
+            # Since we want to minimize volatility, we return it directly without negation
+        else:
+            raise ValueError("Invalid optimization goal specified.")
+        # Update the personal best if needed
+        if optimize_for == 'sharpe ratio' and value < self.best_value:
             self.best_value = value
             self.best_position = np.copy(self.position)
+        elif optimize_for == 'volatility' and value < self.best_value:
+            self.best_value = value
+            self.best_position = np.copy(self.position)
+
         return value
 
     def update_velocity_and_position(self, global_best_position, w, c1, c2):
--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import numpy as np
 from Particle import Particle
 from StockReturns import getStockReturns
 
-# ['AAPL', 'MSFT', 'GOOG', 'GOOGL', 'AMZN', 'TSLA', 'JNJ', 'UNH', 'META', 'V', 'NVDA', 'XOM', 'PG', 'WMT', 'JPM', 'MA', 'LLY', 'HD', 'PFE', 'CVX', 'BAC', 'KO', 'ABBV', 'MRK', 'PEP', 'VZ', 'TMO', 'COST', 'AVGO', 'ABT', 'DHR', 'ORCL', 'MCD', 'ADBE', 'CSCO', 'CMCSA', 'CRM', 'DIS', 'TMUS', 'NKE', 'BMY', 'PM', 'UPS', 'INTC', 'NEE', 'T', 'WFC', 'TXN', 'RTX', 'QCOM']
-
 tickers = ['AAPL', 'MSFT', 'GOOG', 'GOOGL', 'AMZN', 'TSLA']
 start_date = '2020-01-01'
 end_date = '2021-01-01'
@@ -24,10 +22,12 @@ swarm = [Particle(n_assets) for _ in range(n_particles)]
 global_best_value = float('inf')
 global_best_position = None
 
+optimize_goal = input("Enter optimization goal (1) for sharpe ratio or (2) for volatility:")
+
 # Run the PSO algorithm
 for iteration in range(n_iterations):
     for particle in swarm:
-        value = particle.evaluate(daily_returns, risk_free_rate)
+        value = particle.evaluate(daily_returns, risk_free_rate, optimize_goal)
         # Update the global best if needed
         if value < global_best_value:
             global_best_value = value
@@ -35,14 +35,15 @@ for iteration in range(n_iterations):
 
     for particle in swarm:
         particle.update_velocity_and_position(global_best_position, w, c1, c2)
+    
+    if(optimize_goal == '1'):
+        print(f"Iteration {iteration} - Best Sharpe Ratio: {-global_best_value}")
+    elif(optimize_goal == '2'):
+        print(f"Iteration {iteration} - Best Volatility: {global_best_value}")
 
-    print(f"Iteration {iteration} - Best Sharpe Ratio: {-global_best_value}")
-
-# The position of the global best particle gives us the weights of the optimal portfolio
 optimal_weights = global_best_position
 for stock in range(len(tickers)):
             if optimal_weights[stock] > 0.001:
                 print('|' + str(tickers[stock]).rjust(7, ' '), 
-                      '|' + '{:.1f}'.format(100 * round(optimal_weights[stock], 3) ).rjust(11, ' ') + '% |'
-                      )
+                      '|' + '{:.1f}'.format(100 * round(optimal_weights[stock], 3) ).rjust(11, ' ') + '% |')
 print("Optimal Weights:", optimal_weights)
